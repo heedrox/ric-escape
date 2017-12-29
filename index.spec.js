a14bab17a6bdbc94b0461008de545ce6ad355fbd
jmarti <jmarti@theinit.com>
2017-12-29 17:03:42 +0100

    change spec for hello example

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const ricEscape = require('./index.js');
 
 describe('Ric Escape', () => {
-  it('should exist', () => {
+  it('should say hello', () => {
     ricEscape.ricEscape(anAogRequestBuilder().withIntent('welcome').build());
 
     expect(getAogApp().lastAsk).to.equal('Hello!');
