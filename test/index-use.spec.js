ad7a14daa2bbd5382cd02432117fd703c155e4eb
jmarti <jmarti@theinit.com>
2017-12-31 10:59:29 +0100

    refactor use tests

--- test/index-use.spec.js
+++ test/index-use.spec.js
@@ -3,76 +3,45 @@ const ricEscapeData = require('../ric-escape-data').data;
 const scure = require('../scure/scure').buildScureFor(ricEscapeData);
 
 describe('Ric Escape - when using', () => {
-  it('tells you to specify object if no arg', () => {
-    const request = aDfaRequestBuilder()
-      .withIntent('use')
-      .withArgs({ arg: null })
-      .withData({ roomId: 'sala-mandos' })
-      .build();
+  const WRONG_ARG_DATA = [
+    { arg: null, expectedSentence: 'use-noarg', comment: 'no arg' },
+    { arg: 'Cuadro', expectedSentence: 'use-cant', comment: 'object does not exist' },
+    { arg: 'sillas', expectedSentence: 'use-cant', comment: 'object cannot be used' },
+  ];
 
-    ricEscape.ricEscape(request);
+  WRONG_ARG_DATA.forEach((data) => {
+    it(`tells you cannot be used or wrong object when: ${data.comment}`, () => {
+      const request = aDfaRequestBuilder()
+        .withIntent('use')
+        .withArgs({ arg: data.arg })
+        .withData({ roomId: 'sala-mandos' })
+        .build();
 
-    expect(getDfaApp().lastAsk).to.equal(scure.sentences.get('use-noarg'));
+      ricEscape.ricEscape(request);
+
+      expect(getDfaApp().lastAsk).to.equal(scure.sentences.get(data.expectedSentence));
+    });
   });
 
-  it('tells you it cannot be used if object does not exist', () => {
-    const request = aDfaRequestBuilder()
-      .withIntent('use')
-      .withArgs({ arg: 'Cuadro' })
-      .withData({ roomId: 'sala-mandos' })
-      .build();
+  describe('using objects several times', () => {
+    const TEST_DATA = [
+      { usages: null, expectedText: 'Los primeros minutos del diario' },
+      { usages: { 'sala-mandos-diario': 1 }, expectedText: 'Los siguientes minutos del diario' },
+      { usages: { 'sala-mandos-diario': 2 }, expectedText: 'Los últimos minutos del diario' },
+    ];
 
-    ricEscape.ricEscape(request);
+    TEST_DATA.forEach((data) => {
+      it(`responds depending of number of times used ${data.usages && data.usages['sala-mandos-diario']}`, () => {
+        const request = aDfaRequestBuilder()
+          .withIntent('use')
+          .withArgs({ arg: 'diario de abordo' })
+          .withData({ roomId: 'sala-mandos', usages: data.usages })
+          .build();
 
-    expect(getDfaApp().lastAsk).to.equal(scure.sentences.get('use-cant'));
-  });
+        ricEscape.ricEscape(request);
 
-  it('tells you it cannot be used if object has no use', () => {
-    const request = aDfaRequestBuilder()
-      .withIntent('use')
-      .withArgs({ arg: 'sillas' })
-      .withData({ roomId: 'sala-mandos' })
-      .build();
-
-    ricEscape.ricEscape(request);
-
-    expect(getDfaApp().lastAsk).to.equal(scure.sentences.get('use-cant'));
-  });
-
-
-  it('uses objects', () => {
-    const request = aDfaRequestBuilder()
-      .withIntent('use')
-      .withArgs({ arg: 'diario de abordo' })
-      .withData({ roomId: 'sala-mandos' })
-      .build();
-
-    ricEscape.ricEscape(request);
-
-    expect(getDfaApp().lastAsk).to.contains('Los primeros minutos del diario');
-  });
-
-  it('tells another response a second time', () => {
-    const request = aDfaRequestBuilder()
-      .withIntent('use')
-      .withArgs({ arg: 'diario de abordo' })
-      .withData({ roomId: 'sala-mandos', usages: { 'sala-mandos-diario': 1 } })
-      .build();
-
-    ricEscape.ricEscape(request);
-
-    expect(getDfaApp().lastAsk).to.contains('Los siguientes minutos del diario');
-  });
-
-  it('tells another response a third time', () => {
-    const request = aDfaRequestBuilder()
-      .withIntent('use')
-      .withArgs({ arg: 'diario de abordo' })
-      .withData({ roomId: 'sala-mandos', usages: { 'sala-mandos-diario': 2 } })
-      .build();
-
-    ricEscape.ricEscape(request);
-
-    expect(getDfaApp().lastAsk).to.contains('Los últimos minutos del diario');
+        expect(getDfaApp().lastAsk).to.contains(data.expectedText);
+      });
+    });
   });
 });
