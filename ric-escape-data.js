f1ffa5ba5fc7604a6ec38c40d9355953d7385aec
jmarti <jmarti@theinit.com>
2018-01-01 22:08:23 +0100

    allow to use computer only

--- ric-escape-data.js
+++ ric-escape-data.js
@@ -138,6 +138,13 @@ exports.data = {
     anUsage(['combinacion-4815', 'hab108-cajafuerte'], [
       aPickingAction('Clic. Sí, la caja se ha abierto. Hay un aparato extraño dentro de la caja fuerte. Me lo llevo.', 'hab108-aparato'),
     ], true),
+    anUsage(['sala-mandos-ordenador'], [
+      aConditionalResponse([
+        aCondDescUsage(false, '!unlocked:ricmodified', 'No puedo alterar el curso de navegación del ordenador. Mi programación no me deja hacerlo, ya que la única forma de salvar la humanidad es estrellarnos y morir.'),
+        aCondDescUsage(false, '!unlocked:humanitysaved', theEndingScene('Ok, he alterado el curso de navegación, ya no os estrellaréis. Todo termina aquí. Felicidades, has conseguido salvarte, pero no has salvado a la humanidad. Podías haber hecho algo diferente para llegar a este punto. Pero no, has preferido salvarte tú. Lo siento, pero tú y tu raza estáis abocados a la extinción. Adiós.')),
+        aCondDescUsage(false, 'unlocked:humanitysaved', theEndingScene('He alterado el curso de navegación, ya no os estrellaréis. Y además, la humanidad está salvada, ya que el patógeno está muerto por efecto del betacaroteno. ¡Enhorabuena! Has hecho un trabajo excelente. Hasta la próxima.')),
+      ]),
+    ], false),
     anUsage(['ric', 'sala-mandos-ordenador'], [
       aConditionalResponse([
         aCondDescUsage(false, '!unlocked:ricmodified', 'No puedo alterar el curso de navegación del ordenador. Mi programación no me deja hacerlo, ya que la única forma de salvar la humanidad es estrellarnos y morir.'),
