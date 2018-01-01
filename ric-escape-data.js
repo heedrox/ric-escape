139d91a9dd188a211881178648ef201768b69895
jmarti <jmarti@theinit.com>
2018-01-01 17:16:17 +0100

    modify ending text to make it shorter

--- ric-escape-data.js
+++ ric-escape-data.js
@@ -117,7 +117,7 @@ exports.data = {
     anUsage(['ric', 'sala-mandos-ordenador'], [
       aConditionalResponse([
         aCondDesc('!unlocked:ricmodified', 'No quiero alterar el curso de navegación del ordenador, pues es necesario que todos muráis. Solo así salvaremos la humanidad.'),
-        aCondDesc('unlocked:ricmodified', 'Ok, he alterado el curso de navegación, ya no os estrellaréis. Todo termina aquí. Felicidades, has conseguido salvarte, pero no has salvado a la humanidad. Podías haber hecho algo diferente para llegar a este punto y quizás haber salvado la humanidad. Pero no, has preferido salvarte tú. Lo siento, pero tú y tu raza estáis abocados a la extinción. Adiós.'),
+        aCondDesc('unlocked:ricmodified', 'Ok, he alterado el curso de navegación, ya no os estrellaréis. Todo termina aquí. Felicidades, has conseguido salvarte, pero no has salvado a la humanidad. Podías haber hecho algo diferente para llegar a este punto. Pero no, has preferido salvarte tú. Lo siento, pero tú y tu raza estáis abocados a la extinción. Adiós.'),
       ]),
     ], false),
     anUsage(['ric', 'hab108-aparato'], [
